a48d676e3e3ef9df29d29668184332de66249cd9
Simplicity-Sprint <110411267+Simplicity-Sprint@users.noreply.github.com>
2022-12-01 12:54:37 +0000

    Modify index.html

--- ag_datasets/hf_mlm_dataset.py
+++ ag_datasets/hf_mlm_dataset.py
@@ -8,4 +8,8 @@ from pathlib import Path
 
 class AGHFMLMDataset(torch.utils.data.Dataset):
     """Implements a torch Dataset class for Ancient Greek input data that
-        will be used by the HF
+        will be used by the HF Trainer API during training."""
+
+    def __init__(
+            self,
+            
