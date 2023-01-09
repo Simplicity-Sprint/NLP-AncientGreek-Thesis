cb2b5b452fe76b7db3a9fb3870606143976267a9
Simplicity-Sprint <110411267+Simplicity-Sprint@users.noreply.github.com>
2023-01-09 15:03:36 +0000

    Fix selection

--- data_preparation/download_all.py
+++ data_preparation/download_all.py
@@ -5,4 +5,5 @@ from pathlib import Path
 
 
 PLAIN_TEXT_DATA_DIR = Path(__file__).parent.parent/'data'/'plain-text'
-MLM_TARGET_DIR = (PLAIN_TEX
+MLM_TARGET_DIR = (PLAIN_TEXT_DATA_DIR/'MLM').resolve()
+POS_TARGET_DIR = (PLAIN_TEXT_DATA_DIR/'PoS').
