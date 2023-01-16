6a6a3faef6e4738123bf1f2a210f3a9da9a2557b
Simplicity-Sprint <110411267+Simplicity-Sprint@users.noreply.github.com>
2023-01-16 14:21:47 +0000

    Release 2.7.3

--- data_preparation/download_diorisis.py
+++ data_preparation/download_diorisis.py
@@ -12,3 +12,7 @@ from typing import List, Tuple, Dict, Union
 from data_preparation.data_prep_utils import (
     clean_raw_text,
     download_and_unzip,
+    print_stats_and_save,
+    save_pickle
+)
+from data_preparatio
