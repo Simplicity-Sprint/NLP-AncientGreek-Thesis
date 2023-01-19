b63554db6a3abad8104d619dbf8107ec1f2f5560
Simplicity-Sprint <110411267+Simplicity-Sprint@users.noreply.github.com>
2023-01-19 20:09:49 +0000

    house keeping

--- data_preparation/download_diorisis.py
+++ data_preparation/download_diorisis.py
@@ -41,4 +41,5 @@ def get_tokens_and_pos_tags(
         #  whole sentence is broken, skip it
         if token['type'] != 'punct' and \
                 ('lemma' not in token or 'POS' not in token['lemma']):
-          
+            return [], []
+        elif 'lemma' in token and token['lemma
