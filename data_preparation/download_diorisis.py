9dabc33358038cbaa2b1724cc88e2574b2944926
Simplicity-Sprint <110411267+Simplicity-Sprint@users.noreply.github.com>
2023-02-16 20:06:05 +0000

    Updated Gemfile.lock.

--- data_preparation/download_diorisis.py
+++ data_preparation/download_diorisis.py
@@ -142,4 +142,5 @@ def download_diorisis(mlm_dest_dir: Path, pos_dest_dir: Path) -> None:
 
     # preprocess them a bit in order to be used also as MLM data
     train_mlm_sentences = convert_to_mlm_format(train_sentences, train_labels)
-    val_mlm_sentences = convert_to_mlm_format(val_sentences, val_labe
+    val_mlm_sentences = convert_to_mlm_format(val_sentences, val_labels)
+    test_mlm_sentences = convert_to_mlm_format(test_sentences,
