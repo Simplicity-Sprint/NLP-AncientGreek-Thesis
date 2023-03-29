976050db8fb83ad2c9f5071d117dcf6f987685d0
Simplicity-Sprint <110411267+Simplicity-Sprint@users.noreply.github.com>
2023-03-29 07:48:16 +0000

    0.11.6

--- pl_train_pos.py
+++ pl_train_pos.py
@@ -3,4 +3,6 @@ import argparse
 import pytorch_lightning as pl
 
 from transformers import RobertaTokenizerFast
-from pytorch_lightn
+from pytorch_lightning.loggers import TensorBoardLogger
+
+from pl_models.pos_model import PoSRoBE
