656a423552236da65c1a1980cf28028a28532b25
Simplicity-Sprint <110411267+Simplicity-Sprint@users.noreply.github.com>
2023-04-23 06:54:31 +0000

    improve test coverage

--- pl_tune_mlm.py
+++ pl_tune_mlm.py
@@ -28,4 +28,5 @@ BEST_ARGS = None
 def create_and_train_model(
         args: Dict[str, Union[float, int]],
         constants: Dict[str, Union[int, float, bool, Tuple[Path, Path, Path],
-                   
+                                   RobertaTokenizerFast, Path]]
+) -> LitRoBERT
