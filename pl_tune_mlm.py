dfcc7c487b48d745be5509ecddef0e974661c727
Simplicity-Sprint <110411267+Simplicity-Sprint@users.noreply.github.com>
2023-04-23 21:45:06 +0000

    Release 1.0.4

--- pl_tune_mlm.py
+++ pl_tune_mlm.py
@@ -44,4 +44,6 @@ def create_and_train_model(
     # handle logging
     logdir = constants['tb-logdir']
     force_empty_directory(logdir)
-    logger = TensorBoardLogger(str(logdir), name='AG-RoBERTa-Temp', vers
+    logger = TensorBoardLogger(str(logdir), name='AG-RoBERTa-Temp', version=0)
+
+    # train the model (use a good GPU, otherwise it will 
