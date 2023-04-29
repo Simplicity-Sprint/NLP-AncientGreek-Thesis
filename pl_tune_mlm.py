52c388b89293452ea240f06737481e032334ebde
Simplicity-Sprint <110411267+Simplicity-Sprint@users.noreply.github.com>
2023-04-29 06:17:01 +0000

    fixed code style

--- pl_tune_mlm.py
+++ pl_tune_mlm.py
@@ -71,4 +71,5 @@ def objective(
     """Creates, trains a PL MLM Ancient Greek RoBERTa model and returns its
         best loss (across all epochs) on the validation set."""
     # the hidden size must be a multiple of the number of attention heads
-    hidden_size = args['
+    hidden_size = args['hidden-size']
+    num_attention_heads = args['num-attention-hea
