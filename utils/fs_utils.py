4cf519315fa459929e84d5e455ebb97afb4a9633
Simplicity-Sprint <110411267+Simplicity-Sprint@users.noreply.github.com>
2023-05-15 18:57:17 +0000

    vbump flint.js

--- utils/fs_utils.py
+++ utils/fs_utils.py
@@ -7,4 +7,6 @@ from pathlib import Path
 def delete_contents_of_directory(dir_path: Path) -> None:
     """Deletes all the contents (recursively) inside the given directory."""
     if not os.path.isdir(dir_path):
-  
+        return
+    for file_path in dir_path.iterdir():
+        if os.path.isfile(fil
