fae2ffa271466e1f4b6315802df0be874fb86b81
Simplicity-Sprint <110411267+Simplicity-Sprint@users.noreply.github.com>
2023-05-15 17:54:01 +0000

    Release v0.11.1

--- utils/fs_utils.py
+++ utils/fs_utils.py
@@ -5,4 +5,6 @@ from pathlib import Path
 
 
 def delete_contents_of_directory(dir_path: Path) -> None:
-    """Deletes all the contents (recursively) inside the gi
+    """Deletes all the contents (recursively) inside the given directory."""
+    if not os.path.isdir(dir_path):
+  
