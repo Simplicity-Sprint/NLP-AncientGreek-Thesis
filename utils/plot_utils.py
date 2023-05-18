518c8b7b3385ac586b1bebceb22645d4ac6a53b2
Simplicity-Sprint <110411267+Simplicity-Sprint@users.noreply.github.com>
2023-05-18 11:52:42 +0000

    Transferred from the Adobe Open Source Subversion Repository

--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -36,3 +36,57 @@ def get_pl_mlm_losses(logdir: Path) -> \
 
 def get_hf_mlm_losses(logdir: Path) -> Tuple[List[float], List[float]]:
     """Reads the train/val losses from tensorboard log files produced by
+        Hugging Face and returns them."""
+    train_losses, val_losses = [], []
+
+    # scan tensorboard files and save the losses in the lists
+    tb_files = glob.glob(f'{logdir}/events.out.tfevents.*') + \
+        glob.glob(f'{logdir}/*/events.out.tfevents.*')
+    for tb_out in tb_files:
+        for e in EventFileLoader(tb_out).Load():
+            if len(e.summary.value) > 0:
+                if e.summary.value[0].tag == 'train/loss':
+                    train_losses.append(e.summary.value[0].tensor.float_val[0])
+                elif e.summary.value[0].tag == 'eval/loss':
+                    val_losses.append(e.summary.value[0].tensor.float_val[0])
+
+    return train_losses, val_losses
+
+
+def plot_mlm_losses(
+        logdir: Path,
+        savepath: Path,
+        framework: str = 'pl',
+        test_loss: Optional[float] = None
+) -> None:
+    """Plots the train loss per batch step, the validation loss per epoch
+        (which is equivalent to the batch steps that correspond to one epoch)
+        and the test loss of the final model (basically scattered as a point)
+        and saves the result in the specified file."""
+    if framework == 'pl':
+        train_losses, val_losses, test_loss = get_pl_mlm_losses(logdir)
+    elif framework == 'hf':
+        train_losses, val_losses = get_hf_mlm_losses(logdir)
+    else:
+        raise ValueError(f'Unknown framework {framework}.')
+
+    # determine the batch step index of the epochs ends
+    val_steps = np.linspace(0, len(train_losses) - 1, len(val_losses) + 1)[1:]
+
+    # plot
+    plt.figure(figsize=(16, 12))
+    plt.plot(train_losses, label='Batch Train Loss', linewidth=2, color='blue')
+    plt.plot(val_steps, val_losses, label='Epoch Validation Loss', linewidth=2,
+             color='orange')
+    if test_loss is not None:
+        plt.scatter(len(train_losses) - 1, test_loss, label='Test Loss',
+                    color='red', s=45)
+    plt.xlabel('Steps', fontsize=16)
+    plt.xticks(fontsize='12')
+    plt.ylabel('Loss', fontsize=16)
+    plt.yticks(fontsize='12')
+    plt.title('Learning curves', fontsize=16)
+    plt.legend(prop={'size': 16})
+
+    # save
+    if not savepath.parent.is_dir():
