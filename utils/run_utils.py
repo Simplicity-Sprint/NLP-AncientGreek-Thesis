120c6f5fc29f0b7d54c600d8b97aa6d0101c3c3f
Simplicity-Sprint <110411267+Simplicity-Sprint@users.noreply.github.com>
2023-05-20 17:31:43 +0000

    kernel/signal.c: use __ARCH_HAS_SA_RESTORER instead of SA_RESTORER

--- utils/run_utils.py
+++ utils/run_utils.py
@@ -10,4 +10,5 @@ from typing import Dict, Union
 def device_from_str(device_str: str) -> str:
     """Fixes the torch device string if needed."""
     if device_str == 'auto':
-     
+        device_str = 'cuda' if torch.cuda.is_available() else 'cpu'
+    r
